94a9772bf8a7431a50845774db86c2081b81a993
Chris Heaney <chrisheaney30@gmail.com>
2022-05-23 08:24:01 -0500

    improve variable names

--- src/summary.ts
+++ src/summary.ts
@@ -22,6 +22,11 @@ import {
 	Market,
 } from '@drift-labs/sdk';
 
+/**
+ * Calculates how much to increase k given the cost of the operation
+ * @param market
+ * @param cost
+ */
 function calculateBudgetedK(market: Market, cost: BN): [BN, BN] {
 	// wolframalpha.com
 	// (1/(x+d) - p/(x*p+d))*y*d*Q = C solve for p
@@ -30,7 +35,6 @@ function calculateBudgetedK(market: Market, cost: BN): [BN, BN] {
 	// todo: assumes k = x * y
 	// otherwise use: (y(1-p) + (kp^2/(x*p+d)) - k/(x+d)) * Q = C solve for p
 
-	// const k = market.amm.sqrtK.mul(market.amm.sqrtK);
 	const x = market.amm.baseAssetReserve;
 	const y = market.amm.quoteAssetReserve;
 
@@ -38,14 +42,6 @@ function calculateBudgetedK(market: Market, cost: BN): [BN, BN] {
 	const Q = market.amm.pegMultiplier;
 
 	const C = cost.mul(new BN(-1));
-
-	console.log(
-		convertToNumber(x, AMM_RESERVE_PRECISION),
-		convertToNumber(y, AMM_RESERVE_PRECISION),
-		convertToNumber(d, AMM_RESERVE_PRECISION),
-		Q.toNumber() / 1e3,
-		C.toNumber() / 1e6
-	);
 
 	const numer1 = y.mul(d).mul(Q).div(AMM_RESERVE_PRECISION).div(PEG_PRECISION);
 	const numer2 = C.mul(x.add(d)).div(QUOTE_PRECISION);
@@ -61,9 +57,6 @@ function calculateBudgetedK(market: Market, cost: BN): [BN, BN] {
 		.div(AMM_RESERVE_PRECISION)
 		.div(PEG_PRECISION);
 
-	// console.log('numers:', convertToNumber(numer1), convertToNumber(numer2));
-	// console.log('denoms:', convertToNumber(denom1), convertToNumber(denom2));
-
 	const numerator = d
 		.mul(numer1.sub(numer2))
 		.div(AMM_RESERVE_PRECISION)
@@ -73,15 +66,6 @@ function calculateBudgetedK(market: Market, cost: BN): [BN, BN] {
 		.add(denom2)
 		.div(AMM_RESERVE_PRECISION)
 		.div(AMM_TO_QUOTE_PRECISION_RATIO);
-	console.log(numerator, denominator);
-	// const p = (numerator).div(denominator);
-
-	// const formulaCost = numer21
-	// 	.sub(numer20)
-	// 	.sub(numer1)
-	// 	.mul(market.amm.pegMultiplier)
-	// 	.div(AMM_TIMES_PEG_TO_QUOTE_PRECISION_RATIO);
-	// console.log(convertToNumber(formulaCost, QUOTE_PRECISION));
 
 	return [numerator, denominator];
 }
@@ -93,12 +77,11 @@ const getSummary = async (
 ) => {
 	await clearingHouse.subscribe();
 
-	// await updateUserAccounts();
 	let allQuoteAcq = 0;
 	let allQuoteAcqLong = 0;
 	let allQuoteAcqShort = 0;
-	let alltFMD = 0;
-	let alltF = 0;
+	let allTotalFeeMinusDistributions = 0;
+	let allTotalFee = 0;
 
 	const result = [];
 
@@ -110,17 +93,14 @@ const getSummary = async (
 		await clearingHouse.program.account.userPositions.all();
 	const programUserAccounts = await clearingHouse.program.account.user.all();
 
-	console.log(programUserPositionAccounts.length, 'user positions');
-
-	const positionPnL = Array(Markets.length).fill(0);
+	const localUserPositionsPnl = Array(Markets.length).fill(0);
 	const positionLongCostBasis = Array(Markets.length).fill(0);
 	const positionShortCostBasis = Array(Markets.length).fill(0);
 
 	const markets = clearingHouse.getMarketsAccount().markets;
 
 	for (const positionsStr in programUserPositionAccounts) {
-		const positions1 = programUserPositionAccounts[positionsStr];
-		const positions = positions1.account.positions as UserPosition[];
+		const positions = programUserPositionAccounts[positionsStr].account.positions as UserPosition[];
 		for (const positionStr in positions) {
 			const position = positions[positionStr];
 			if (position.baseAssetAmount.eq(ZERO)) {
@@ -134,9 +114,8 @@ const getSummary = async (
 				true
 			);
 			const posPnlNum = convertToNumber(posPnl, QUOTE_PRECISION);
-			// console.log(posPnlNum)
 
-			positionPnL[posMarketIndex] += posPnlNum;
+			localUserPositionsPnl[posMarketIndex] += posPnlNum;
 
 			if (position.baseAssetAmount.gt(ZERO)) {
 				positionLongCostBasis[posMarketIndex] += convertToNumber(
@@ -149,27 +128,16 @@ const getSummary = async (
 					QUOTE_PRECISION
 				);
 			}
-
-			// const userPositionDesc = {
-			// 	posPnl: posPnlNum,
-			// 	marketIndex: posMarketIndex,
-			// 	authority: positions1.account.authority,
-			// };
 		}
-
-		// userPosResult.push(userPositionDesc);
 	}
 
-	console.log(programUserAccounts.length, 'users');
-
-	let totalUserCollateral = 0;
-	// let totalUserCollateralL = 0;
-	let totalPnl1 = 0; // net @ terminal
-	let totalPnl2 = 0; // cur @ local
-	let totalPnl2Swap = 0; //  net @ terminal (two swap)
+	let userCollateralSum = 0;
+	let localUserPnl = 0; // cur @ local
+	let terminalUserPnl1 = 0; // net @ terminal
+	let terminalUserPnl2 = 0; //  net @ terminal (two swap)
 
 	for (const programUserAccount of programUserAccounts) {
-		totalUserCollateral += convertToNumber(
+		userCollateralSum += convertToNumber(
 			// @ts-ignore
 			programUserAccount.account.collateral,
 			QUOTE_PRECISION
@@ -185,16 +153,6 @@ const getSummary = async (
 			continue;
 		}
 		const market0M = clearingHouse.getMarket(marketIndex);
-		console.log(Markets[marketIndex].symbol);
-
-		// const netMarketBaseAssetAmountNum = convertToNumber(
-		// 	market0.baseAssetAmount,
-		// 	AMM_RESERVE_PRECISION
-		// );
-		// const totalOINum = convertToNumber(
-		// 	market0.baseAssetAmountLong.sub(market0.baseAssetAmountShort),
-		// 	AMM_RESERVE_PRECISION
-		// );
 
 		const baseAssetNum = convertToNumber(
 			market0.baseAssetAmount,
@@ -214,7 +172,6 @@ const getSummary = async (
 			positionShortCostBasis[marketIndex]
 		);
 		let netQuoteAssetAmount;
-		// netQuoteAssetAmount = Math.abs((longQuoteAssetAmount+shortQuoteAssetAmount)*(netMarketBaseAssetAmountNum/totalOINum));
 
 		if (baseAssetNum > 0) {
 			netQuoteAssetAmount = longQuoteAssetAmount - shortQuoteAssetAmount;
@@ -230,7 +187,6 @@ const getSummary = async (
 				netQuoteAssetAmount * QUOTE_PRECISION.toNumber()
 			),
 			openOrders: ZERO,
-			// quoteAssetAmount: new BN(0),
 		};
 		const netUserLongPosition = {
 			baseAssetAmount: market0.baseAssetAmountLong,
@@ -240,7 +196,6 @@ const getSummary = async (
 				longQuoteAssetAmount * QUOTE_PRECISION.toNumber()
 			),
 			openOrders: ZERO,
-			// quoteAssetAmount: new BN(0),
 		};
 		const netUserShortPosition = {
 			baseAssetAmount: market0.baseAssetAmountShort,
@@ -250,7 +205,6 @@ const getSummary = async (
 				shortQuoteAssetAmount * QUOTE_PRECISION.toNumber()
 			),
 			openOrders: ZERO,
-			// quoteAssetAmount: new BN(0),
 		};
 
 		const reKCost = market0M.amm.totalFeeMinusDistributions;
@@ -266,7 +220,8 @@ const getSummary = async (
 			.mul(kNumer)
 			.div(kDenom);
 
-		const quoteAssetPnL = calculatePositionPNL(
+		// First way to calculate the terminal pnl
+		const terminalUserPositionPnl = calculatePositionPNL(
 			market0M,
 			netUserPosition,
 			false
@@ -311,7 +266,7 @@ const getSummary = async (
 				netUserPosition.baseAssetAmount.abs(),
 				getSwapDirection('base', directionToClose)
 			);
-		const terminalPrice2 = newQuoteAssetReserve
+		const terminalPrice = newQuoteAssetReserve
 			.mul(MARK_PRICE_PRECISION)
 			.mul(market0.amm.pegMultiplier)
 			.div(PEG_PRECISION)
@@ -322,42 +277,6 @@ const getSummary = async (
 
 		const marketForShorts = Object.assign({}, market0);
 		marketForShorts.amm = Object.assign({}, market0.amm);
-
-		// console.log('current price:', roundDecimal(convertToNumber(calculateMarkPrice(market0M))));
-		// console.log('long value:', convertToNumber(calculateBaseAssetValue(market0M, netUserLongPosition),  QUOTE_PRECISION));
-
-		// const [newQuoteAssetReserveLongClosed1, newBaseAssetReserveLongClosed1] = calculateAmmReservesAfterSwap(
-		//     market0M.amm,
-		//     'base',
-		//     netUserLongPosition.baseAssetAmount.abs(),
-		//     getSwapDirection('base', PositionDirection.SHORT)
-		// );
-
-		// marketForShorts.amm.baseAssetReserve = newBaseAssetReserveLongClosed1;
-		// marketForShorts.amm.quoteAssetReserve = newQuoteAssetReserveLongClosed1;
-		// const terminalPriceLongClosed1 = newQuoteAssetReserveLongClosed1
-		// .mul(MARK_PRICE_PRECISION)
-		// .mul(market0.amm.pegMultiplier)
-		// .div(PEG_PRECISION)
-		// .div(newBaseAssetReserveLongClosed1);
-
-		// console.log('long closed price:', roundDecimal(convertToNumber(terminalPriceLongClosed1)));
-
-		// console.log('short value:', convertToNumber(calculateBaseAssetValue(market0M, netUserShortPosition), QUOTE_PRECISION));
-
-		// const [newQuoteAssetReserveShortClosed2, newBaseAssetReserveShortClosed2] = calculateAmmReservesAfterSwap(
-		//     marketForShorts.amm,
-		//     'base',
-		//     netUserShortPosition.baseAssetAmount.abs(),
-		//     getSwapDirection('base', PositionDirection.LONG)
-		// );
-		// const terminalPriceShortClosed2 = newQuoteAssetReserveShortClosed2
-		// .mul(MARK_PRICE_PRECISION)
-		// .mul(market0.amm.pegMultiplier)
-		// .div(PEG_PRECISION)
-		// .div(newBaseAssetReserveShortClosed2);
-
-		// console.log('short closed price:', roundDecimal(convertToNumber(terminalPriceShortClosed2)));
 
 		const longPnl = calculatePositionPNL(
 			marketForLongs,
@@ -371,6 +290,7 @@ const getSummary = async (
 				netUserLongPosition.baseAssetAmount.abs(),
 				getSwapDirection('base', PositionDirection.SHORT)
 			);
+
 		marketForShorts.amm.baseAssetReserve = baseAssetReserveForLongs;
 		marketForShorts.amm.quoteAssetReserve = quoteAsserReserveForShorts;
 		const shortPnl = calculatePositionPNL(
@@ -378,28 +298,28 @@ const getSummary = async (
 			netUserShortPosition,
 			false
 		);
-		const quoteAssetPnL2Swap = convertToNumber(
+		const terminalUserPositionPnl2 = convertToNumber(
 			longPnl.add(shortPnl),
 			QUOTE_PRECISION
 		);
 
-		const tF = convertToNumber(market0M.amm.totalFee, QUOTE_PRECISION);
-		const tFMD = convertToNumber(
+		const totalFee = convertToNumber(market0M.amm.totalFee, QUOTE_PRECISION);
+		const totalFeeMinusDistributions = convertToNumber(
 			market0M.amm.totalFeeMinusDistributions,
 			QUOTE_PRECISION
 		);
-		alltFMD += tFMD;
-		alltF += tF;
+		allTotalFeeMinusDistributions += totalFeeMinusDistributions;
+		allTotalFee += totalFee;
 
 		// terminal pnl
-		const pnl1 = convertToNumber(quoteAssetPnL, QUOTE_PRECISION);
-		totalPnl1 += pnl1;
+		const terminalUserPositionPnlNum = convertToNumber(terminalUserPositionPnl, QUOTE_PRECISION);
+		terminalUserPnl1 += terminalUserPositionPnlNum;
 
-		totalPnl2Swap += quoteAssetPnL2Swap;
+		terminalUserPnl2 += terminalUserPositionPnl2;
 
 		// local pnl
-		const pnl2 = positionPnL[marketIndex];
-		totalPnl2 += pnl2;
+		const localUserPositionPnl = localUserPositionsPnl[marketIndex];
+		localUserPnl += localUserPositionPnl;
 
 		const marketDesc = {
 			marketSymbol: Markets[marketIndex].symbol,
@@ -412,14 +332,14 @@ const getSummary = async (
 			quotePaidShort: roundDecimal(shortQuoteAssetAmount),
 
 			// terminal pnl
-			quotePnL: roundDecimal(pnl1),
+			terminalPnl1: roundDecimal(terminalUserPositionPnlNum),
+			terminalPnl2: roundDecimal(terminalUserPositionPnl2),
 			// local pnl
-			quotePnL2: roundDecimal(pnl2),
-			quotePnL2Swap: roundDecimal(quoteAssetPnL2Swap),
+			localPnl: roundDecimal(localUserPositionPnl),
 
 			exitPrice: convertToNumber(exitPrice, QUOTE_PRECISION),
 			terminalPrice: roundDecimal(
-				convertToNumber(terminalPrice2, MARK_PRICE_PRECISION)
+				convertToNumber(terminalPrice, MARK_PRICE_PRECISION)
 			),
 			peg: roundDecimal(
 				convertToNumber(market0M.amm.pegMultiplier, PEG_PRECISION)
@@ -464,14 +384,14 @@ const getSummary = async (
 		collateralVault,
 		insuranceVault,
 		collateralVaultPlusInsuranceVaultBalance,
-		totalUserCollateral: totalUserCollateral,
-		totalUserCollateralTerminal: totalUserCollateral + totalPnl1,
-		totalUserCollateralTerminal2: totalUserCollateral + totalPnl2Swap,
-		totalUserCollateralLocal: totalUserCollateral + totalPnl2,
-		allTotalFee: alltF,
-		allTotalFeeMinusDistributions: alltFMD,
+		userCollateralSum,
+		totalUserCollateralTerminal: userCollateralSum + terminalUserPnl1,
+		totalUserCollateralTerminal2: userCollateralSum + terminalUserPnl2,
+		totalUserCollateralLocal: userCollateralSum + localUserPnl,
+		allTotalFee: allTotalFee,
+		allTotalFeeMinusDistributions: allTotalFeeMinusDistributions,
 		systemSurplus:
-			collateralVaultPlusInsuranceVaultBalance - (totalUserCollateral + totalPnl1 + alltFMD),
+			collateralVaultPlusInsuranceVaultBalance - (userCollateralSum + terminalUserPnl1 + allTotalFeeMinusDistributions),
 	};
 
 	result.push(aggDesc);
